37cbc0958001dbf9edc6a443ba602f7f976b2469
IvarLannister <Siegeofparis@gmail.com>
2022-06-18 04:53:34 -0400

    just updated the file now the stopwatch should work like intended lol

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 let seconds = 00;
 let tens = 00;
-let appendTens = document.getElementById('tens');
-let appendSecondsx = document.getElementById('seconds');
+let appendTens = document.getElementById("tens");
+let appendSeconds = document.getElementById("seconds");
+let buttonFirst= document.getElementById("button-first");
+let buttonSecond = document.getElementById("button-second");
+let buttonThird = document.getElementById("button-third");
+let interval; // to store timer values
+
+
+// this function will run when click on start
+
+function startTimer(){
+    tens++;
+
+
+    if (tens < 9 ) {
+        appendTens.innerHTML = "0" + tens;
+    }
+    if (tens > 9) {
+        appendTens.innerHTML = tens;
+    } 
+    if (tens>99) {
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+    }
+
+    if (seconds > 9) {
+        appendSeconds.innerHTML = seconds; 
+    }
+}
+
+buttonFirst.onclick = function () {
+    interval = setInterval(startTimer);
+};
+
+buttonSecond.onclick = function() {
+    clearInterval(interval);
+};
+
+buttonThird.onclick = function(){
+    clearInterval(interval);
+    tens = "00";
+    seconds = "00";
+    appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
+}
